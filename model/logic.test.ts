f96de75130fd3fbbafc40b5418a7bfcc6eed3c90
Touko Vainio-Kaila <>
2021-04-06 11:08:16 +0300

    Add logic

--- model/logic.test.ts
+++ model/logic.test.ts
@@ -1,9 +1,248 @@
+import * as exampleConfig from "../example-config.json";
 import { getRecommendation } from "./logic";
+import { Recommendation } from "./types";
+
+const recommend = getRecommendation(exampleConfig);
 
 describe("Calculation logic", () => {
-  it("should return something", () => {
-    const recommendation = getRecommendation(150);
+  describe("for lengths outside applicable ranges", () => {
+    it("should return undefined for too small length", () => {
+      const recommendation = recommend(50);
 
-    expect(recommendation).toBeTruthy();
+      expect(recommendation).toBeUndefined();
+    });
+
+    it("should return undefined for too big length", () => {
+      const recommendation = recommend(300);
+
+      expect(recommendation).toBeUndefined();
+    });
+  });
+
+  describe("for adult lengths", () => {
+    it("should return right results", () => {
+      const recommendation = recommend(150);
+
+      const expected: Recommendation = {
+        type: "only-sizes",
+        sizes: {
+          classic: {
+            poles: {
+              min: 150,
+              max: 150 * 1.1,
+            },
+            skis: {
+              min: 150 + 5,
+              max: 150 + 6,
+            },
+          },
+          skating: {
+            poles: {
+              min: 150 * 1.1,
+              max: 150 * 1.2,
+            },
+            skis: {
+              min: 150 + 7,
+              max: 150 + 8,
+            },
+          },
+        },
+      };
+      expect(recommendation).toEqual(expected);
+    });
+  });
+
+  describe("for children lengths", () => {
+    it("should return right row for an exact length match", () => {
+      const recommendation = recommend(100);
+
+      const expected: Recommendation = {
+        type: "only-table-rows",
+        rows: [
+          {
+            childLength: 100,
+            skiLength: 101,
+            poleLength: 102,
+          },
+        ],
+      };
+      expect(recommendation).toEqual(expected);
+    });
+
+    it("should return row with smallest length for an exact lowest length", () => {
+      const recommendation = recommend(90);
+
+      const expected: Recommendation = {
+        type: "only-table-rows",
+        rows: [
+          {
+            childLength: 90,
+            skiLength: 91,
+            poleLength: 92,
+          },
+        ],
+      };
+      expect(recommendation).toEqual(expected);
+    });
+
+    it("should return the row with smallest length for a length lower than lowest row", () => {
+      const recommendation = recommend(89);
+
+      const expected: Recommendation = {
+        type: "only-table-rows",
+        rows: [
+          {
+            childLength: 90,
+            skiLength: 91,
+            poleLength: 92,
+          },
+        ],
+      };
+      expect(recommendation).toEqual(expected);
+    });
+
+    it("should return upper and lower row for a length in between", () => {
+      const recommendation101 = recommend(101);
+      const recommendation109 = recommend(109);
+
+      const expected: Recommendation = {
+        type: "only-table-rows",
+        rows: [
+          {
+            childLength: 100,
+            skiLength: 101,
+            poleLength: 102,
+          },
+          {
+            childLength: 110,
+            skiLength: 111,
+            poleLength: 112,
+          },
+        ],
+      };
+      expect(recommendation101).toEqual(expected);
+      expect(recommendation109).toEqual(expected);
+    });
+  });
+
+  describe("for lengths using both adult & children lengths", () => {
+    it("should return both rows and calculated lengths", () => {
+      const recommendation = recommend(120);
+
+      const expected: Recommendation = {
+        type: "both",
+        rows: [
+          {
+            childLength: 120,
+            skiLength: 121,
+            poleLength: 122,
+          },
+        ],
+        sizes: {
+          classic: {
+            poles: {
+              min: 120,
+              max: 120 * 1.1,
+            },
+            skis: {
+              min: 120 + 5,
+              max: 120 + 6,
+            },
+          },
+          skating: {
+            poles: {
+              min: 120 * 1.1,
+              max: 120 * 1.2,
+            },
+            skis: {
+              min: 120 + 7,
+              max: 120 + 8,
+            },
+          },
+        },
+      };
+      expect(recommendation).toEqual(expected);
+    });
+
+    it('should return both rows and calculated lengths also at the upper limit of "both"', () => {
+      const recommendation = recommend(125);
+
+      const expected: Recommendation = {
+        type: "both",
+        rows: [
+          {
+            childLength: 120,
+            skiLength: 121,
+            poleLength: 122,
+          },
+        ],
+        sizes: {
+          classic: {
+            poles: {
+              min: 125,
+              max: 125 * 1.1,
+            },
+            skis: {
+              min: 125 + 5,
+              max: 125 + 6,
+            },
+          },
+          skating: {
+            poles: {
+              min: 125 * 1.1,
+              max: 125 * 1.2,
+            },
+            skis: {
+              min: 125 + 7,
+              max: 125 + 8,
+            },
+          },
+        },
+      };
+      expect(recommendation).toEqual(expected);
+    });
+
+    it('should return both rows and calculated lengths also at the lower limit of "both"', () => {
+      const recommendation = recommend(115);
+
+      const expected: Recommendation = {
+        type: "both",
+        rows: [
+          {
+            childLength: 110,
+            skiLength: 111,
+            poleLength: 112,
+          },
+          {
+            childLength: 120,
+            skiLength: 121,
+            poleLength: 122,
+          },
+        ],
+        sizes: {
+          classic: {
+            poles: {
+              min: 115,
+              max: 115 * 1.1,
+            },
+            skis: {
+              min: 115 + 5,
+              max: 115 + 6,
+            },
+          },
+          skating: {
+            poles: {
+              min: 115 * 1.1,
+              max: 115 * 1.2,
+            },
+            skis: {
+              min: 115 + 7,
+              max: 115 + 8,
+            },
+          },
+        },
+      };
+      expect(recommendation).toEqual(expected);
+    });
   });
 });
--- model/logic.ts
+++ model/logic.ts
@@ -1,27 +1,116 @@
-import { Recommendation } from "./types";
+import {
+  AdultConfig,
+  ChildTableRow,
+  Config,
+  MinMax,
+  Recommendation,
+  Sizes,
+} from "./types";
 
-export const getRecommendation = (length: number): Recommendation => ({
-  type: "only-sizes",
-  sizes: {
-    classic: {
-      poles: {
-        min: length + 1,
-        max: length + 2,
-      },
-      skis: {
-        min: 1,
-        max: 2,
-      },
+type ResultType = "children" | "adults" | "both";
+
+type Recommender = (length: number) => Recommendation | undefined;
+
+export const getRecommendation = (config: Config): Recommender => {
+  const { adultConfig, childConfig } = config;
+
+  const childRowsSorted = [...childConfig.tableRows].sort(
+    (r1, r2) => r1.childLength - r2.childLength
+  );
+
+  return (length: number) => {
+    const type = determineResultType(config, length);
+
+    if (!type) {
+      return undefined;
+    }
+
+    if (type === "children") {
+      return {
+        type: "only-table-rows",
+        rows: findChildRows(childRowsSorted, length),
+      };
+    }
+    if (type === "both") {
+      return {
+        type: "both",
+        rows: findChildRows(childRowsSorted, length),
+        sizes: calculateSizes(adultConfig, length),
+      };
+    }
+    return {
+      type: "only-sizes",
+      sizes: calculateSizes(adultConfig, length),
+    };
+  };
+};
+
+const determineResultType = (
+  { adultConfig, childConfig }: Config,
+  length: number
+): ResultType | undefined => {
+  const withinChild = isWithin(length, childConfig.applicableFor);
+  const withinAdult = isWithin(length, adultConfig.applicableFor);
+  if (withinAdult && withinChild) {
+    return "both";
+  } else if (withinChild) {
+    return "children";
+  } else if (withinAdult) {
+    return "adults";
+  } else {
+    return undefined;
+  }
+};
+
+const isWithin = (length: number, applicableFor: MinMax) =>
+  length >= applicableFor.min && length <= applicableFor.max;
+
+const calculateSizes = (adultConfig: AdultConfig, length: number): Sizes => ({
+  classic: {
+    poles: {
+      min: length * adultConfig.poleMultipliers.classic.min,
+      max: length * adultConfig.poleMultipliers.classic.max,
     },
-    skating: {
-      poles: {
-        min: length + 1,
-        max: length + 2,
-      },
-      skis: {
-        min: 1,
-        max: 2,
-      },
+    skis: {
+      min: length + adultConfig.skiAdditions.classic.min,
+      max: length + adultConfig.skiAdditions.classic.max,
+    },
+  },
+  skating: {
+    poles: {
+      min: length * adultConfig.poleMultipliers.skating.min,
+      max: length * adultConfig.poleMultipliers.skating.max,
+    },
+    skis: {
+      min: length + adultConfig.skiAdditions.skating.min,
+      max: length + adultConfig.skiAdditions.skating.max,
     },
   },
 });
+
+const findChildRows = (
+  tableRowsSorted: ChildTableRow[],
+  length: number
+): ChildTableRow[] => {
+  const firstLargerOrEqIndex = tableRowsSorted.findIndex(
+    (row) => row.childLength >= length
+  );
+  if (firstLargerOrEqIndex === -1) {
+    // All rows have length smaller than given length -> return last
+    return [tableRowsSorted[tableRowsSorted.length - 1]];
+  }
+
+  const firstLargerOrEq = tableRowsSorted[firstLargerOrEqIndex];
+  if (firstLargerOrEqIndex === 0) {
+    // Length smaller or eq to row with smallest length
+    return [firstLargerOrEq];
+  }
+
+  if (firstLargerOrEq.childLength === length) {
+    // Exact match -> only matching row
+    return [firstLargerOrEq];
+  }
+
+  // In between two rows -> return those
+  return [tableRowsSorted[firstLargerOrEqIndex - 1], firstLargerOrEq];
+};
